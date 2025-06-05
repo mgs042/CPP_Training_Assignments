9c9d460127372fa6d28ce00c7e221a3c6e1bd226
M Gouri Sankar <mundancherygourisankar@gmail.com>
2025-06-05 10:26:42 +0530

    day38

---
+++ Assignments/Day38/Duplicate Word Frequency/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<set>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string text = "hello world hello code hello learn world code";
+	size_t start = 0, end;
+	while (start = text.find(' ') != string::npos)
+	{
+
+	}
+}
--- Assignments/Day38/Score Tracker/main.cpp
+++ Assignments/Day38/Score Tracker/main.cpp
@@ -1,40 +1,38 @@
 #include<iostream>
 #include<set>
+#include<string>
 using namespace std;
 
 int main()
 {
 	multiset<int> scores;
 	string s = "";
-	int val;
+	int val=-1;
 	while (s != "exit")
 	{
+		getline(cin, s);
 		if (s.find("ADD ") != string::npos)
 			scores.insert(atoi(s.substr(4).c_str()));
 		else if (s == "TOP3")
 		{
 			auto pos = scores.rbegin();
-			for (int i = 0; i < 3; ++i)
+			for (int i = 0; i < 3 && pos != scores.rend(); ++i)
 			{
 				if (*pos != val)
 				{
 					val = *pos;
 					cout << val << " ";
+					i--;
 				}
-				pos = prev(pos - 1);
+				pos++;
 			}
+			cout << endl;
 		}
 		else if (s.find("COUNT ") != string::npos)
-			scores.count(atoi(s.substr(6).c_str()));
+			cout << scores.count(atoi(s.substr(6).c_str())) << endl;
 		else if (s.find("REMOVE ") != string::npos)
 		{
-			for (auto i : scores)
-			{
-				if (i == atoi(s.substr(7).c_str()))
-				{
-
-				}
-			}
+			scores.erase(atoi(s.substr(7).c_str()));
 		}
 	}
 }
