9fe83f4ff99eae2f64d7d9413dc556d0b9374cf8
M Gouri Sankar <mundancherygourisankar@gmail.com>
2025-06-05 10:56:22 +0530

    day 38

--- Assignments/Day38/Duplicate Word Frequency/main.cpp
+++ Assignments/Day38/Duplicate Word Frequency/main.cpp
@@ -6,9 +6,18 @@ using namespace std;
 int main()
 {
 	string text = "hello world hello code hello learn world code";
+	multiset<string>words;
 	size_t start = 0, end;
-	while (start = text.find(' ') != string::npos)
+	while ((end=text.find(' ', start)) != string::npos)
 	{
-
+		words.insert(text.substr(start, end-start));
+		start = end+1;
 	}
+	words.insert(text.substr(start));
+	for (auto i : words)
+		cout << i << " ";
+	cout << endl;
+	cout << "Count of 'hello': " << words.count("hello") <<endl;
+	cout << "Count of 'world': " << words.count("world")<<endl;
+	cout << "Count of 'code': " << words.count("code")<<endl;
 }
