e15d45e9d31377726cb32930f81ee1fca8f84da1
M Gouri Sankar <mundancherygourisankar@gmail.com>
2025-06-11 19:07:42 +0530

    day44

--- Classwork/Day44/Problems/prob6.cpp
+++ Classwork/Day44/Problems/prob6.cpp
@@ -1,3 +1,36 @@
 #include<iostream>
 #include<thread>
 #include<mutex>
+
+std::mutex mtx;
+
+void oddEven(bool flag)
+{
+	for (static int i = 1; i <= 20;)
+	{
+		std::lock_guard<std::mutex> lock(mtx);
+		if (i == 21)
+			break;
+		if (flag == true && i%2==0)
+		{
+			std::cout << i << " ";
+			i++;
+		}
+		else if (flag == false && i % 2 != 0)
+		{
+			std::cout << i << " ";
+			i++;
+		}
+	}
+}
+
+int main()
+{
+	std::thread t1(oddEven, true);
+	std::thread t2(oddEven, false);
+	if (t1.joinable())
+		t1.join();
+	if (t2.joinable())
+		t2.join();
+	return 0;
+}
